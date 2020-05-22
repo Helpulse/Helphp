deee473fc66f7c0aa1af43e016218c3dba6191eb
Mauro Leandro Pasqual <mauropasqual@hotmail.com>
2020-05-22 09:57:44 -0300

    phpmyadmin

---
+++ config.class.php
@@ -0,0 +1,9 @@
+﻿<?php
+	class config {
+		protected $mysqli;
+		
+		public function __construct() {
+			$this->mysqli = new mysqli('http://185.27.134.10/','epiz_25823569','xVIXoExaris','epiz_25823569_database');
+		}
+	}
+?>
