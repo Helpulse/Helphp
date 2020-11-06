d4b15435f33e7597fe872c29b20b23fda74b2c39
Helpulse <65712599+Helpulse@users.noreply.github.com>
2020-11-06 08:08:32 -0300

    Delete config.class.php

--- config.class.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-	class config {
-		protected $mysqli;
-		
-		public function __construct() {
-			$this->mysqli = new mysqli('http://185.27.134.10/','epiz_25823569','xVIXoExaris','epiz_25823569_database');
-		}
-	}
-?>
